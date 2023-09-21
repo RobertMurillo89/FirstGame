13a66a4889f48d4f35eca9e86dc9670da68a8faf
Rillo813 <robert.murillo.89@gmail.com>
2023-09-20 22:07:57 -0700

    Update EnemyAI1.cs

--- Assets/Scripts/EnemyAI/EnemyAI1.cs
+++ Assets/Scripts/EnemyAI/EnemyAI1.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+    public Transform player;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
