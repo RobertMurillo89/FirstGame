9e069d8bd2a887f1c809729dd1bcdd59ea1adb48
Rillo813 <robert.murillo.89@gmail.com>
2023-09-20 23:33:47 -0700

    I dont remember what i did. OH yeah started a new enemy ai. its broken.

--- Assets/Scripts/EnemyAI/EnemyAI1.cs
+++ Assets/Scripts/EnemyAI/EnemyAI1.cs
@@ -8,6 +8,7 @@ public class EnemyAI1 : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
 
+    public LayerMask whatIsGround, whatIsPlayer;
     //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
@@ -23,7 +24,70 @@ public class EnemyAI1 : MonoBehaviour
 
     private void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>(); 
+    }
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && !playerInSightRange) AttackPlayer();
     }
 
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkPoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack Code Here
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 }
