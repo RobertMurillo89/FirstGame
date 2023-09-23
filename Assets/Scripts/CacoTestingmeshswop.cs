11687593cbc4cbd35c5b3a58f21611aaa136547c
bigpallidbat <82740920+bigpallidbat@users.noreply.github.com>
2023-09-22 20:31:24 -0700

    push some caco code

--- Assets/Scripts/CacoTestingmeshswop.cs
+++ Assets/Scripts/CacoTestingmeshswop.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class CacoTestingmeshswop : MonoBehaviour
+public class CacoTestingmeshswop : MonoBehaviour, IDamage
 {
+    [Header("----- Components -----")]
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("----- Stats -----")]
+    [SerializeField] int HP;
+
+
+    [Header("----- Attack -----")]
+
+
+    [Header("----- Movement -----")]
+    [SerializeField] AudioClip WakeUp;
+    [SerializeField] AudioClip Attack;
+    [SerializeField] AudioClip Pain;
+    [SerializeField] AudioClip Death;
+    AudioSource CacoSounds;
+    [SerializeField] int Speed;
     [SerializeField] GameObject Idel;
     [SerializeField] GameObject Attack1;
     [SerializeField] GameObject Attack2;
@@ -16,6 +35,8 @@ public class CacoTestingmeshswop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CacoSounds = GetComponent<AudioSource>();
+        GameManager.instance.updateGameGoal(1);
         Attack1.SetActive(false);
         Attack2.SetActive(false);
         Attack3.SetActive(false);
@@ -30,41 +51,34 @@ public class CacoTestingmeshswop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        agent.SetDestination(GameManager.instance.player.transform.position);
     }
-    void rollcall()
+    public void TakeDamage(int amount)
     {
-        while (true)
+        HP -= amount;
+        MPain1();
+        //StartCoroutine(flashDamage());
+
+        if (HP <= 0)
         {
-            Invoke("MIdel", 1);
-            Invoke("MAtt1", 1);
-            Invoke("MAtt2", 1);
-            Invoke("MAtt3", 1);
-            Invoke("MIdel", 1);
-            Invoke("MPain1", 1);
-            Invoke("MPain1f", 1);
-            Invoke("MPain2", 1);
-            Invoke("MPain2f", 1);
-            Invoke("MPain1", 1);
-            //Invoke("MIdel", 0.285714f);
-            //Invoke("MAtt1", 0.142857f);
-            //Invoke("MAtt2", 0.142857f);
-            //Invoke("MAtt3", 0.142857f);
-            //Invoke("MIdel", 0.285714f);
-            //Invoke("MPain1", 0.028571f);
-            //Invoke("MPain1f", 0.028571f);
-            //Invoke("MPain2", 0.028571f);
-            //Invoke("MPain2f", 0.028571f);
-            //Invoke("MPain1", 0.057142f);
+            GameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
         }
     }
+    IEnumerator flashDamage()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+
     void MIdel()
     {
         Attack3.SetActive(false);
         Pain1.SetActive(false);
         Pain2f.SetActive(false);
-        Idel.SetActive(true); 
-        Invoke("MAtt1", 0.142857f);
+        Idel.SetActive(true);
+        //Invoke("MAtt1", 0.142857f);
     }
 
     void MAtt1()
@@ -90,6 +104,8 @@ public class CacoTestingmeshswop : MonoBehaviour
 
     void MPain1()
     {
+        CacoSounds.PlayOneShot(Pain);
+        Idel.SetActive(false);
         Attack1.SetActive(false);
         Attack2.SetActive(false);
         Attack3.SetActive(false);
@@ -119,7 +135,13 @@ public class CacoTestingmeshswop : MonoBehaviour
     {
         Pain2.SetActive(false);
         Pain2f.SetActive(true);
-        Invoke("MIdel", 0.285714f);
+        Invoke("MPainend", 0.028571f);
+    }
+    void MPainend()
+    {
+        Pain2f.SetActive(false);
+        Pain1.SetActive(true);
+        Invoke("MIdel", 0.057142f);
     }
 
 }
