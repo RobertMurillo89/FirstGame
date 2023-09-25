661d9c19b22cc5dc7b2f47fc69de45a2c4391f65
bigpallidbat <82740920+bigpallidbat@users.noreply.github.com>
2023-09-25 09:04:48 -0700

    fixed  red caco projectile fully

--- Assets/Scripts/Items/CcoProjectile.cs
+++ Assets/Scripts/Items/CcoProjectile.cs
@@ -20,6 +20,7 @@ public class CcoProjectile : MonoBehaviour
     [SerializeField] GameObject Death4;
     [SerializeField] GameObject Death5;
     [SerializeField] GameObject Death6;
+    public bool didHit = false;
     void Start()
     {
         Destroy(gameObject, destroyTime);
@@ -31,25 +32,33 @@ public class CcoProjectile : MonoBehaviour
     void MFly1()
     {
         Fly2.SetActive(false);
-        Fly1.SetActive(true);
-        Invoke("MFly2", 0.057142f);
+        if (!didHit)
+        {
+            Fly1.SetActive(true);
+            Invoke("MFly2", 0.057142f);
+        }
     }
 
     void MFly2()
     {
         Fly1.SetActive(false);
-        Fly2.SetActive(true);
-        Invoke("MFly1", 0.057142f);
+        if (!didHit)
+        {
+            Fly2.SetActive(true);
+            Invoke("MFly1", 0.057142f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        MDeath1();
         IDamage damageable = other.GetComponent<IDamage>();
         if (!other.isTrigger)
         {
+            //&& !didHit
             if (damageable != null && other.gameObject.CompareTag("Player"))
             {
+                didHit = true;
+                MDeath1();
                 damageable.TakeDamage(damage);
             }
         }
@@ -57,7 +66,7 @@ public class CcoProjectile : MonoBehaviour
     }
     void MDeath1()
     {
-        rb.velocity = new Vector3(0,0,0);
+        rb.velocity = new Vector3(0, 0, 0);
         CacoSounds.PlayOneShot(Hit);
         Fly2.SetActive(false);
         Fly1.SetActive(false);
@@ -67,36 +76,64 @@ public class CcoProjectile : MonoBehaviour
 
     void MDeath2()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
         Death1.SetActive(false);
         Death2.SetActive(true);
         Invoke("MDeath3", 0.057142f);
     }
     void MDeath3()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
+        Death1.SetActive(false);
         Death2.SetActive(false);
         Death3.SetActive(true);
         Invoke("MDeath4", 0.057142f);
     }
     void MDeath4()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
+        Death1.SetActive(false);
+        Death2.SetActive(false);
         Death3.SetActive(false);
         Death4.SetActive(true);
         Invoke("MDeath5", 0.057142f);
     }
     void MDeath5()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
+        Death1.SetActive(false);
+        Death2.SetActive(false);
+        Death3.SetActive(false);
         Death4.SetActive(false);
         Death5.SetActive(true);
         Invoke("MDeath6", 0.057142f);
     }
     void MDeath6()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
+        Death1.SetActive(false);
+        Death2.SetActive(false);
+        Death3.SetActive(false);
+        Death4.SetActive(false);
         Death5.SetActive(false);
         Death6.SetActive(true);
         Invoke("MDeath", 0.057142f);
     }
     void MDeath()
     {
+        Fly2.SetActive(false);
+        Fly1.SetActive(false);
+        Death1.SetActive(false);
+        Death2.SetActive(false);
+        Death3.SetActive(false);
+        Death4.SetActive(false);
+        Death5.SetActive(false);
+        Death6.SetActive(false);
         Destroy(gameObject);
     }
 }
